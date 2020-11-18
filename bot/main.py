f9efda0078418fed0bf8499ea68c7ad55a43c058
KirillTregubov <imkirilltregubov@gmail.com>
2020-11-18 02:04:05 -0500

    Updated

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,8 @@
 import os
+import discord
 from discord.ext import commands
 
+client = discord.Client()
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -10,7 +12,12 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("pong")
+    await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))
+
+@client.event
+async def on_message(message):
+    if isinstance(message.channel, discord.channel.DMChannel) and message.author != client.user:
+        await message.channel.send('This is a DM')
 
 if __name__ == "__main__":
     bot.run(TOKEN)
