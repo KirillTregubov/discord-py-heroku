22bc6ea84ec48637d52a1280e5090e9ae671200c
KirillTregubov <imkirilltregubov@gmail.com>
2020-12-08 22:44:47 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,8 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 client = discord.Client()
 bot = commands.Bot(command_prefix="!")
